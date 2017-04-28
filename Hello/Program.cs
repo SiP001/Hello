d76fb10c49ae1555bd05e556ced0b2da4e454e2f
phair <phair@DESKTOP-IET6C83>
2017-04-28 22:41:44 +0100

    Added logic to calculate the day suffix for todays date

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,8 +10,27 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            string GetDaySuffix(int day)
+            {
+                switch (day)
+                {
+                    case 1:
+                    case 21:
+                    case 31:
+                        return "st";
+                    case 2:
+                    case 22:
+                        return "nd";
+                    case 3:
+                    case 23:
+                        return "rd";
+                    default:
+                        return "th";
+                }
+            }
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Today is " + DateTime.Today.DayOfWeek + " the " + DateTime.Today.Day + " of " + DateTime.Now.ToString("MMMM"));
+            
+            Console.WriteLine("Today is " + DateTime.Today.DayOfWeek + " the " + DateTime.Today.Day + GetDaySuffix(DateTime.Today.Day) + " of " + DateTime.Now.ToString("MMMM"));
         }
     }
 }
